201c4584064fabff14fe2922b5c0a479acde0426
Roy Sizemore <rsizemore@swampcastle.home>
2021-03-09 15:32:16 -0500

    Added visibility functionality to cards and cleaned up callback functions

--- script.js
+++ script.js
@@ -23,13 +23,13 @@ for (i = 0; i < 5; i++) {
 
 // Pairs up weather condition codes with icon codes/filenames and sets icon URL up to wildcard position
 const weatherIcons = {
-  "Clear": "01d",
-  "Clouds": "02d",
-  "Drizzle": "09d",
-  "Rain": "10d",
-  "Thunderstorm": "11d",
-  "Snow": "13d",
-  "Atmosphere": "50d",
+  'Clear': '01d',
+  'Clouds': '02d',
+  'Drizzle': '09d',
+  'Rain': '10d',
+  'Thunderstorm': '11d',
+  'Snow': '13d',
+  'Atmosphere': '50d',
 };
 
 let iconURL = "<img src='http://openweathermap.org/img/wn/";
@@ -43,25 +43,26 @@ const getWeather = () => {
   }).then((response) => {
     // Empties each card; adds date +1; adds temp and humidity, then finds the appropriate condition in weatherIcons object and adds the icon
     $('.card').empty();
+    $('.card').css('visibility', 'visible');
     $('#0').append(moment().add(1, 'days').format('MM/DD'));
-    $('#0').append("</br>" + response.list[6].main.temp + "° F");
-    $('#0').append("</br>" + response.list[6].main.humidity + "% humidity");
+    $('#0').append('</br>' + response.list[6].main.temp + '° F');
+    $('#0').append('</br>' + response.list[6].main.humidity + '% humidity');
     $('#0').append(iconURL + weatherIcons[response.list[6].weather[0].main] + ".png'>");
     $('#1').append(moment().add(2, 'days').format('MM/DD'));
-    $('#1').append("</br>" + response.list[14].main.temp + "° F");
-    $('#1').append("</br>" + response.list[14].main.humidity + "% humidity");
+    $('#1').append('</br>' + response.list[14].main.temp + '° F');
+    $('#1').append('</br>' + response.list[14].main.humidity + '% humidity');
     $('#1').append(iconURL + weatherIcons[response.list[14].weather[0].main] + ".png'>");
     $('#2').append(moment().add(3, 'days').format('MM/DD'));
-    $('#2').append("</br>" + response.list[22].main.temp + "° F");
-    $('#2').append("</br>" + response.list[22].main.humidity + "% humidity");
+    $('#2').append('</br>' + response.list[22].main.temp + '° F');
+    $('#2').append('</br>' + response.list[22].main.humidity + '% humidity');
     $('#2').append(iconURL + weatherIcons[response.list[22].weather[0].main] + ".png'>");
     $('#3').append(moment().add(4, 'days').format('MM/DD'));
-    $('#3').append("</br>" + response.list[30].main.temp + "° F");
-    $('#3').append("</br>" + response.list[30].main.humidity + "% humidity");
+    $('#3').append('</br>' + response.list[30].main.temp + '° F');
+    $('#3').append('</br>' + response.list[30].main.humidity + '% humidity');
     $('#3').append(iconURL + weatherIcons[response.list[30].weather[0].main] + ".png'>");
     $('#4').append(moment().add(5, 'days').format('MM/DD'));
-    $('#4').append("</br>" + response.list[38].main.temp + "° F");
-    $('#4').append("</br>" + response.list[38].main.humidity + "% humidity");
+    $('#4').append('</br>' + response.list[38].main.temp + '° F');
+    $('#4').append('</br>' + response.list[38].main.humidity + '% humidity');
     $('#4').append(iconURL + weatherIcons[response.list[38].weather[0].main] + ".png'>");
   });
 
@@ -69,7 +70,7 @@ const getWeather = () => {
     // Retrieves today's weather
     url: `https://api.openweathermap.org/data/2.5/weather?q=${$searchInput.val()},us&units=imperial&appid=9b1d4def9f4c5a84bc5a47775e26390d`,
     method: 'GET',
-    success: function(data) {
+    success: (data) => {
       // Gets latitude and longitude for use in UVI API
       lat = data.coord.lat;
       lon = data.coord.lon;
@@ -79,58 +80,62 @@ const getWeather = () => {
       $.ajax({
         url: `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=9b1d4def9f4c5a84bc5a47775e26390d`,
         method: 'GET',
-        success: function(data) {
+        success: (data) => {
         localStorage.setItem('uv_index', data.value)}
       });
 
       // Clears current city (if any), adds new city/current date
       $('#city-title').remove();
-      $contain1.prepend($('<h2>').text(response.name + ' ' + moment().format("MM/DD/YY")).attr('id', 'city-title').addClass('w-100'));
+      $contain1.prepend($('<h2>').text(response.name + ' ' + moment().format('MM/DD/YY')).attr('id', 'city-title').addClass('w-100'));
       
       // Gets UVI from localStorage and sets status (low/mod/high)
       let uv_index = localStorage.getItem('uv_index');
       let uv_stat;
       if (uv_index <= 2) {
-        uv_stat = "low";
+        uv_stat = 'low';
       } else if (uv_index >2 && uv_index <=5) {
-        uv_stat = "moderate";
+        uv_stat = 'moderate';
       } else {
-        uv_stat = "high";
+        uv_stat = 'high';
       };
 
-      // Clears all current statuses, 
+      // Clears all current statuses and updates all current day statuses with icon
       $('#stats').remove();
       $contain1.append($('<div>').attr('id', 'stats').addClass('w-100'));
       $('#stats').append($('<textarea readonly>').text(`Temperature: ${response.main.temp}° F (feels like ${response.main.feels_like}° F)\nHumidity: ${response.main.humidity}%\nWind Speed: ${response.wind.speed} mph`));
       $('#stats').append(`<div class='box' id='index-box'>UV Index: ${uv_index} (${uv_stat})</div>`);
-      $('#stats').append(`<div class='box' id='now-icon' + ".png'>`);
+      $('#stats').append(`<div class='box' id='now-icon' + '.png'>`);
       $('#now-icon').append(iconURL + weatherIcons[response.weather[0].main] + ".png'>");
       
-      if (uv_stat === "low") {
+      // Sets colour of box for UVI
+      if (uv_stat === 'low') {
         $('#index-box').css('background-color', 'darkgreen')
-      } else if (uv_stat === "moderate") {
+      } else if (uv_stat === 'moderate') {
         $('#index-box').css('background-color', 'goldenrod')
       } else {
         $('#index-box').css('background-color', 'red')
       };
 
+      // Checks localStorage for previously entered locations. If not already in localStorage, sets location in localStorage. Makes each location a clicable button that runs getWeather function again for that location
       if (!(response.name in localStorage)) {
           localStorage.setItem(response.name, response.name);
           $searchCol.append($('<button>').addClass('w-100 searchHist').text(response.name).attr('id', response.name))
           };
       });
 
+      // Clears search box after search is submitted
       $searchInput.val('')
 };
 
-$btn.on('click', () => {getWeather(), getForecast()});
+// Runs the function on click, Enter and click on history button
+$btn.on('click', () => {getWeather()});
 $searchInput.on('keypress', (e) => {
   if (e.which === 13) {
     getWeather();
   };
 });
 
-$searchCol.on('click', '.searchHist', function() {
+$searchCol.on('click', '.searchHist', () => {
   $searchInput.val($(this).text());
   getWeather();
 });
